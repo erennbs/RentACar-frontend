1d097f419d9c31fa3f6fe0d44e24d86f7cb902ab
Eren <erenbas3453@gmail.com>
2024-08-13 19:59:27 +0300

    Remove Sidebar Component and Add Admin Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,18 +9,24 @@ import { CarEditComponent } from './components/car-edit/car-edit.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './guards/auth.guard';
+import { adminGuard } from './guards/admin.guard';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RentalService } from './services/rental.service';
+import { RentalsComponent } from './components/rentals/rentals.component';
 
 export const routes: Routes = [
     {path: "", component: CarsComponent},
     {path: "cars", component: CarsComponent},
     {path: "login", component: LoginComponent},
     {path: "register", component: RegisterComponent},
+    {path: "profile", component: ProfileComponent, canActivate: [authGuard]},
     {path: "cars/add", component: CarAddComponent, canActivate: [authGuard]},
     {path: "cars/brand/:brandId", component: CarsComponent},
     {path: "cars/color/:colorId", component: CarsComponent},
     {path: "cars/:carId", component: CarDetailsComponent},
-    {path: "cars/:carId/edit", component: CarEditComponent},
+    {path: "cars/:carId/edit", component: CarEditComponent, canActivate: [adminGuard]},
     {path: "cars/:carId/rent", component: RentComponent, canActivate: [authGuard]},
-    {path: "brands", component: BrandsComponent},
-    {path: "colors", component: ColorsComponent}
+    {path: "brands", component: BrandsComponent, canActivate: [adminGuard]},
+    {path: "colors", component: ColorsComponent, canActivate: [adminGuard]},
+    {path: "rentals", component: RentalsComponent, canActivate: [adminGuard]}
 ];
---
+++ src/app/guards/admin.guard.ts
@@ -0,0 +1,11 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  if (inject(AuthService).isAdmin()) {
+    return true;
+  }
+  inject(Router).navigateByUrl('cars');
+  return false;
+};
